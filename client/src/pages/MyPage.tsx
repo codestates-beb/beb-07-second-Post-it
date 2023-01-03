4e0fdd0d559c2734b63e8d70c0ce5571a7bd7539
mamem12 <mamem.dev@gmail.com>
2023-01-03 20:59:31 +0900

    MyPage 작성

--- client/src/pages/MyPage.tsx
+++ client/src/pages/MyPage.tsx
@@ -1,14 +1,15 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
-// user : users,
-// post : posts,
-// nft : nfts
+import "./MyPage.css"
 
 function MyPage() {
     const [user, setUser] = useState({address : "", created_at : "", eth_amount : 0, nickname : "", token_amount: 0, id : 0})
     const [postList, setPostList] = useState([{id : 0, user_id : 0, title : "", content : "", created_at : "", views : 0}])
-    
+    const [NFTList, setNFTList] = useState([])
+    const [isSet, setIsSet] = useState(true)
+
+
     useEffect(() => {
         axios.get(`http://localhost:4000/users/mypage/${sessionStorage.getItem("user_id")}`)
         .then((response) => {
@@ -17,8 +18,15 @@ function MyPage() {
             setUser(response.data.user)
         })
          
-        console.log("마이페이지 요청")
     }, [])
+
+    function PostListClick(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+        setIsSet(true)
+    }
+
+    function NFTListClick(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+        setIsSet(false)
+    }
 
     return(
         <main>
@@ -27,20 +35,42 @@ function MyPage() {
             <p>Address : {user.address}</p>
             <p>ETH : {user.eth_amount}</p>
             <p>TOKEN : {user.token_amount}</p>
+            
+            <nav className='mypage_nav'>
+                <ul>
+                    <li>
+                        <a onClick={e => PostListClick(e)}><b>POST LIST</b></a>
+                    </li>
+                    <li>
+                        <a onClick={e => NFTListClick(e)}><i>NFT LIST</i></a>
+                    </li>
+                </ul>
+            </nav>
             <hr />
 
-            {postList.map((item) => {
-                return (
-                    <div>
+
+            {isSet ? 
+                postList.map((item) => {
+                    return (
+                        <div>
+                            
+                            <a href={`/post/detail?id=${item.id}`}><h3>{item.title}</h3></a>        
+                            <p>Created At : {item.created_at}</p>
+                            <p>Views : {item.views}</p>
+                        <hr />
                         
-                        <a href={`/post/detail?id=${item.id}`}><h3>{item.title}</h3></a>        
-                        <p>Created At : {item.created_at}</p>
-                        <p>Views : {item.views}</p>
-                    <hr />
-                    
-                    </div>
-                )
-            })}
+                        </div>
+                    )
+                })
+                :
+                NFTList.map((item) => {
+                    return (
+                        <div>
+                            {item}
+                        </div>
+                    )
+                })
+            }
 
 
         </main>
