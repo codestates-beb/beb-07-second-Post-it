0ba43841889307dd000c333b52c0af75bc7dd6c0
mamem12 <mamem.dev@gmail.com>
2023-01-03 20:27:00 +0900

    mint 페이지 작성

--- client/src/pages/Mint.tsx
+++ client/src/pages/Mint.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
+
+// import "./Mint.css"
 
 function Mint() {
+    const [uri, setUri] = useState("");
+
+    function setURI(e: React.ChangeEvent<HTMLInputElement>): void {
+        setUri(e.target.value)
+    }
+
+    async function submit(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+        // await axios.post("", uri);
+        console.log("click submit button")
+    }
     return (
         <main>
-            Mint 페이지 입니다.
+            <section>
+                <blockquote>
+                    <p>
+                        <h1>Minting</h1>
+                        <input placeholder='URI' onChange={e => setURI(e)} className="input__uri" />
+                        <button onClick={e => submit(e)}><b>Create</b></button>
+                    </p>
+                </blockquote>
+            </section>
         </main>
     )
 }
