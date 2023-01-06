590b7f5e2cc970f546a5528365b0f2137892ea2f
bajnsk <baekjunseok63@gmail.com>
2023-01-06 11:38:43 +0900

    contract edit

--- server/src/init.ts
+++ server/src/init.ts
@@ -1,11 +1,13 @@
 import web3 from "./config/web3";
 import AppDataSource from "./db/data-source";
 import user from "./entity/user";
+import bytecode from "./erc20byte";
 
 async function create_server () {
     const accounts = await web3.eth.getAccounts();
     const serverAddress = accounts[0];
     let eth_amount = await web3.eth.getBalance(serverAddress);
+    const abi721 = require("./erc20abi.json");
 
     const users = await AppDataSource
         .getRepository(user)
@@ -16,8 +18,18 @@ async function create_server () {
 
     if(users) {
         console.log("서버 지갑 이미 있습니다.");
+        /* console.log(bytecode); */
     }
     else {
+        var test = new web3.eth.Contract(abi721).deploy(bytecode).send({
+            from: serverAddress, 
+            gas: 4700000
+        })
+
+        const contractAddress = (await test).options.address;
+        const testContract = new web3.eth.Contract(abi721, contractAddress);
+        let erc20amount = await testContract.methods.balanceOf(serverAddress).call();
+
         const info = {
             nickname : "server",
             password : "secret",
