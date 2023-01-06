f01ff88331a2d38254d550b53e809cddc9619497
bajnsk <baekjunseok63@gmail.com>
2023-01-06 14:28:55 +0900

    erc721 contract

--- server/src/init.ts
+++ server/src/init.ts
@@ -1,13 +1,15 @@
 import web3 from "./config/web3";
 import AppDataSource from "./db/data-source";
 import user from "./entity/user";
-import bytecode from "./erc20byte";
+import erc20byte from "./erc20byte";
+import erc721byte from "./erc721byte";
 
 async function create_server () {
     const accounts = await web3.eth.getAccounts();
     const serverAddress = accounts[0];
     let eth_amount = await web3.eth.getBalance(serverAddress);
-    const abi721 = require("./erc20abi.json");
+    const abi20 = require("./erc20abi.json");
+    const abi721 = require("./erc721abi.json");
 
     const users = await AppDataSource
         .getRepository(user)
@@ -21,19 +23,26 @@ async function create_server () {
         /* console.log(bytecode); */
     }
     else {
-        
+
+        var erc721Deploy = new web3.eth.Contract(abi721).deploy(erc721byte).send({
+            from: serverAddress,
+            gas: 4700000
+        })
+        const erc721contArr = (await erc721Deploy).options.address;
+        const erc721Contract = new web3.eth.Contract(abi721, erc721contArr);
+
         // 컨트랙트 배포
-        var test = new web3.eth.Contract(abi721).deploy(bytecode).send({
+        var erc20Deploy = new web3.eth.Contract(abi20).deploy(erc20byte).send({
             from: serverAddress, 
             gas: 4700000
         })
 
         // 컨트랙트 주소가져오기
-        const contractAddress = (await test).options.address;
-        const testContract = new web3.eth.Contract(abi721, contractAddress);
+        const erc20contArr = (await erc20Deploy).options.address;
+        const erc20Contract = new web3.eth.Contract(abi20, erc20contArr);
 
         // 컨트랙트 메소드 호출
-        let erc20amount = await testContract.methods.balanceOf(serverAddress).call();
+        let erc20amount = await erc20Contract.methods.balanceOf(serverAddress).call();
 
         // 트랜잭션 생성
         const info = {
@@ -52,6 +61,8 @@ async function create_server () {
         .save(userss)
         .then((data) => {
             console.log("서버계정을 생성해주었습니다.!");
+            console.log(erc721contArr)
+            console.log(erc20contArr)
         })
         .catch((err) => console.log(err));
     }
