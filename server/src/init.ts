6373833c4574d875dc2da9edcc45248d3a19c12e
mamem12 <mamem.dev@gmail.com>
2023-01-11 18:22:37 +0900

    erc721 settoken완료

--- server/src/init.ts
+++ server/src/init.ts
@@ -1,3 +1,4 @@
+import { json } from "stream/consumers";
 import web3 from "./config/web3";
 import AppDataSource from "./db/data-source";
 import user from "./entity/user";
@@ -6,37 +7,34 @@ import erc721byte from "./erc721byte";
 const abi20 = require("./erc20abi.json");
 const abi721 = require("./erc721abi.json");
 require("dotenv").config()
+// import contractAddress from "./config/addressConfig";
 
 async function create_server () {
     const accounts = await web3.eth.getAccounts();
     const serverAddress = accounts[0];
     let eth_amount = await web3.eth.getBalance(serverAddress);
-
     const users = await AppDataSource
         .getRepository(user)
         .createQueryBuilder()
         .select()
         .where("address = :address", {address:serverAddress})
         .getOne()
-
+        
     if(users) {
         console.log("서버 지갑 이미 있습니다.");
-    }
-    else {
-        let erc721Deploy = new web3.eth.Contract(abi721).deploy(erc721byte).send({
-            from: serverAddress,
-            gas: 4700000
-        })
-        const erc721contArr = (await erc721Deploy).options.address;
-        // const erc721Contract = new web3.eth.Contract(abi721, erc721contArr);
-
-        let erc20Deploy = new web3.eth.Contract(abi20).deploy(erc20byte).send({
+    } else {
+        
+        let erc20Deploy = await new web3.eth.Contract(abi20).deploy(erc20byte).send({
             from: serverAddress, 
             gas: 4700000
         })
 
-        const erc20contArr = (await erc20Deploy).options.address;
-        const erc20Contract = new web3.eth.Contract(abi20, erc20contArr);
+
+        process.env.CONTRACT20_ADDRESS = erc20Deploy.options.address;
+        console.log(process.env.CONTRACT20_ADDRESS)
+        
+
+        const erc20Contract = new web3.eth.Contract(abi20, process.env.CONTRACT20_ADDRESS);
 
         const getTokenAmount = await erc20Contract.methods.balanceOf(serverAddress).call();
 
@@ -48,10 +46,34 @@ async function create_server () {
             eth_amount : String(eth_amount),
         }
 
+        let erc721Deploy = await new web3.eth.Contract(abi721).deploy(erc721byte).send({
+            from: serverAddress,
+            gas: 4700000
+        })
+        
+        process.env.CONTRACT721_ADDRESS = erc721Deploy.options.address;
+        console.log(process.env.CONTRACT721_ADDRESS)
+        const erc721Contract = new web3.eth.Contract(abi721, process.env.CONTRACT721_ADDRESS);
+
+        const data = erc721Contract.methods.setToken(process.env.CONTRACT721_ADDRESS).encodeABI();
+        console.log(process.env.CONTRACT721_ADDRESS)
+        const setSignTokenTx = await web3.eth.accounts.signTransaction({
+
+            from : serverAddress,
+            to : process.env.CONTRACT721_ADDRESS,
+            gas : 500000,
+            data : data
+
+        }, String(process.env.SERVER_PRIVATE_KEY));
+
+        if (setSignTokenTx.rawTransaction) {
+            await web3.eth.sendSignedTransaction(setSignTokenTx.rawTransaction);
+        }
+
         // 디비에 저장
         const userRepo = AppDataSource.getRepository(user);
         const userss = userRepo.create(info);
-    ``
+    
         await userRepo
         .save(userss)
         .then((data) => {
